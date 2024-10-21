84e2f422819470021eb559c04fbf7c42df9426a7
NataliZero <ball.diamond.ru@gmail.com>
2024-10-21 11:24:03 +0300

    add second code
    Вы полнила дз1

--- task_1.py
+++ task_1.py
@@ -1,6 +1,10 @@
 import requests
 
+# Отправляем GET-запрос
 response = requests.get('https://api.github.com/search/repositories?q=html')
 
+# Проверяем статус-код
 print(response.status_code)
 
+# Выводим содержимое ответа в формате JSON
+print(response.json())
